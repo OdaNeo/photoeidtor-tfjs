301b176d03b5e2ac1b616c78c1d985f76adef52c
jinpingliu_iherb <jinping.liu@iherb.com>
2021-12-08 10:01:51 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./index.css";
+import { ImageEditor } from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <ImageEditor
+        includeUI={{
+          loadImage: {
+            path: logo,
+            name: "SampleImage",
+          },
+          uiSize: {
+            width: "1200px",
+            height: "700px",
+          },
+          menuBarPosition: "bottom",
+        }}
+        cssMaxHeight={500}
+        cssMaxWidth={700}
+        selectionStyle={{
+          cornerSize: 20,
+          rotatingPointOffset: 70,
+        }}
+      />
+      <div
+        style={{
+          marginTop: "12px",
+        }}
+      >
+        Upload/Download, ZoomIn/Out, Resize, Clop, Rotate, Text/Draw,
+        Filter(All), Undo/Redo
+      </div>
+    </>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
