e43dc9b5002061a423a84275d3792fe8d50e9057
odaneo <odaneo@outlook.com>
2021-12-23 20:11:23 +0800

    fix: adf

--- src/ps.tsx
+++ src/ps.tsx
@@ -6,9 +6,10 @@ import formatUtil from './utils/format'
 function PS(): JSX.Element {
   const [api, setApi] = useState('')
   const [html, setHtml] = useState('')
+
   const [xApi, setXApi] = useState('14c5630d22f0427e9a82ba16c7570093')
   const [auth, setAuth] = useState(
-    'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LTEuY2VyIn0.eyJpZCI6IjE2NDAxNDM5NzU0MDFfYzIxMGY0NjctMDI1Ni00MDI3LWJmMzgtNmU5NmZjNDFiMDdmX3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiIxNGM1NjMwZDIyZjA0MjdlOWE4MmJhMTZjNzU3MDA5MyIsInVzZXJfaWQiOiIwQkQzMzI3MjYxQjNFOUY3MEE0OTVGQkFAdGVjaGFjY3QuYWRvYmUuY29tIiwiYXMiOiJpbXMtbmExIiwiYWFfaWQiOiIwQkQzMzI3MjYxQjNFOUY3MEE0OTVGQkFAdGVjaGFjY3QuYWRvYmUuY29tIiwiZmciOiJXQk5SSVpDT0ZMRTVJN1VDSE1SRlJIUUEyST09PT09PSIsIm1vaSI6ImIzOWZiYjQzIiwiZXhwaXJlc19pbiI6Ijg2NDAwMDAwIiwiY3JlYXRlZF9hdCI6IjE2NDAxNDM5NzU0MDEiLCJzY29wZSI6Im9wZW5pZCxBZG9iZUlELHJlYWRfb3JnYW5pemF0aW9ucyJ9.KGb9IVje1tZlit_haEbwsuJnQPPFDsh1vnCFE4-eSbsZnWiHRhvY3bHPJrnnjjb6q4Zv1e30KWBO4j9nPv8sMmKz9-rf2T7d6XV0zSQQw3EnlAM4O56xxqlowkKWi3l0ub9NpdyZfAYISkSUFqrxCv0Yrq5tUqFwgpkjGOJbCOMEPGhW0Qf6totCJ0a7uqeR642yTfw8EzjCP08_PS_EihCA25Eanu5W2YX3sc2t5fzG7EUV37YO-t8ZG-6rZ8ca8JP4nvc5Zq0ZOCChMSb4SVN9MJao0GHPDQFS0Rv50EOcIqmwrRmzg2yxH7PCtsjOKDD5De6YKju8ODfEb8mozQ'
+    'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LTEuY2VyIn0.eyJpZCI6IjE2NDAyNjEzODg3MTdfNzhkM2M2NTMtNDNkOC00NzYzLTljMjctZGU2NjkzZmI0MDg0X3VlMSIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiIxNGM1NjMwZDIyZjA0MjdlOWE4MmJhMTZjNzU3MDA5MyIsInVzZXJfaWQiOiIwQkQzMzI3MjYxQjNFOUY3MEE0OTVGQkFAdGVjaGFjY3QuYWRvYmUuY29tIiwiYXMiOiJpbXMtbmExIiwiYWFfaWQiOiIwQkQzMzI3MjYxQjNFOUY3MEE0OTVGQkFAdGVjaGFjY3QuYWRvYmUuY29tIiwiZmciOiJXQlJMVFVaSEZMRTVJN1VDSE1SRlJIUUEyST09PT09PSIsIm1vaSI6IjEyMDgzNWYxIiwiZXhwaXJlc19pbiI6Ijg2NDAwMDAwIiwic2NvcGUiOiJvcGVuaWQsQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMiLCJjcmVhdGVkX2F0IjoiMTY0MDI2MTM4ODcxNyJ9.OyevwJ6gLvh0sPpmlFfsJ5tzDh0xBnDXGOpDG-7fmsC53gofylekLZHJzEheXO42FbhsBc1Ut-xsdsUUESYKc8YfLa3XEdOerbeAhZsqPQflqkY941cBJabhy8beI7AOJ4FvuWdjhywZIeqkvjAryEu5Rpe1hjxBawImV63tXBdp1Rez4fQAOspK6Ju8ecgrJR1EhEkgECI9hbcWg5d6ORmO0LGWsZvJXgjNkNvCFcdj_tYlu7jfg6Iq0stI9K2iX8IC_9erbnoIr83zgl5vug68JQkCiI7clpSE6qk2bOqMUE3Al7tTHWn_KigZpmm_klsHZzj02zoGHwXy7vqlAg'
   )
   const [azureSAS, setAzureSAS] = useState(
     '?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-01-08T09:08:53Z&st=2021-12-21T01:08:53Z&spr=https,http&sig=ewv%2FRf02HNUMBNcOVAKmArWyAsjEzPN3h3L69Kyycbw%3D'
@@ -22,121 +23,113 @@ function PS(): JSX.Element {
     }
   })
 
+  instance.interceptors.response.use(
+    res => {
+      setHtml(formatUtil.objToHTML(res.data))
+      setApi(res.data._links.self.href)
+      return res
+    },
+    err => {
+      setHtml(formatUtil.objToHTML(err.toJSON()))
+    }
+  )
+
   const handleRemoveBg = () => {
     setApi('')
     setHtml('')
-    instance
-      .post('https://image.adobe.io/sensei/cutout', {
-        input: {
-          storage: 'azure',
-          href: 'https://odaneo.blob.core.windows.net/psd/1.png'
+    instance.post('https://image.adobe.io/sensei/cutout', {
+      input: {
+        storage: 'azure',
+        href: 'https://odaneo.blob.core.windows.net/psd/1.png'
+      },
+      options: {
+        optimize: 'performance',
+        process: {
+          postprocess: true
         },
-        options: {
-          optimize: 'performance',
-          process: {
-            postprocess: true
-          },
-          service: {
-            version: '2.5'
-          }
-        },
-        output: {
-          href: `https://odaneo.blob.core.windows.net/psd/2.png${azureSAS}`,
-          storage: 'azure',
-          type: 'image/png',
-          overwrite: true
+        service: {
+          version: '2.5'
         }
-      })
-      .then(res => {
-        setHtml(formatUtil.objToHTML(res.data))
-        setApi(res.data._links.self.href)
-      })
+      },
+      output: {
+        href: `https://odaneo.blob.core.windows.net/psd/2.png${azureSAS}`,
+        storage: 'azure',
+        type: 'image/png',
+        overwrite: true
+      }
+    })
   }
 
   const handleGetLayerInfo = () => {
     setApi('')
     setHtml('')
-    instance
-      .post('https://image.adobe.io/pie/psdService/documentManifest', {
-        inputs: [
-          {
-            href: 'https://odaneo.blob.core.windows.net/psd/1.psd',
-            storage: 'azure'
-          }
-        ]
-      })
-      .then(res => {
-        setApi(res.data._links.self.href)
-      })
+    instance.post('https://image.adobe.io/pie/psdService/documentManifest', {
+      inputs: [
+        {
+          href: 'https://odaneo.blob.core.windows.net/psd/1.psd',
+          storage: 'azure'
+        }
+      ]
+    })
   }
 
   const handleCreate = () => {
     setApi('')
     setHtml('')
-    instance
-      .post('https://image.adobe.io/pie/psdService/smartObject', {
-        inputs: [
+    instance.post('https://image.adobe.io/pie/psdService/smartObject', {
+      inputs: [
+        {
+          href: 'https://odaneo.blob.core.windows.net/psd/1.psd',
+          storage: 'azure'
+        }
+      ],
+      options: {
+        layers: [
           {
-            href: 'https://odaneo.blob.core.windows.net/psd/1.psd',
-            storage: 'azure'
-          }
-        ],
-        options: {
-          layers: [
-            {
-              name: 'New',
-              add: {
-                insertTop: true
-              },
-              input: {
-                href: 'https://odaneo.blob.core.windows.net/psd/1.png',
-                storage: 'azure'
-              }
+            name: 'New',
+            add: {
+              insertTop: true
+            },
+            input: {
+              href: 'https://odaneo.blob.core.windows.net/psd/1.png',
+              storage: 'azure'
             }
-          ]
-        },
-        outputs: [
-          {
-            storage: 'azure',
-            href: `https://odaneo.blob.core.windows.net/psd/2.psd${azureSAS}`,
-            type: 'vnd.adobe.photoshop',
-            overwrite: true
           }
         ]
-      })
-      .then(res => {
-        setApi(res.data._links.self.href)
-      })
+      },
+      outputs: [
+        {
+          storage: 'azure',
+          href: `https://odaneo.blob.core.windows.net/psd/2.psd${azureSAS}`,
+          type: 'vnd.adobe.photoshop',
+          overwrite: true
+        }
+      ]
+    })
   }
 
   const handleAutotone = () => {
     setApi('')
     setHtml('')
-    instance
-      .post('https://image.adobe.io/lrService/autoTone', {
-        inputs: {
-          href: 'https://odaneo.blob.core.windows.net/psd/1.png',
-          storage: 'azure'
-        },
-        outputs: [
-          {
-            href: `https://odaneo.blob.core.windows.net/psd/3.png${azureSAS}`,
-            type: 'image/png',
-            storage: 'azure',
-            overwrite: true
-          }
-        ]
-      })
-      .then(res => {
-        setApi(res.data._links.self.href)
-      })
+    instance.post('https://image.adobe.io/lrService/autoTone', {
+      inputs: {
+        href: 'https://odaneo.blob.core.windows.net/psd/1.png',
+        storage: 'azure'
+      },
+      outputs: [
+        {
+          href: `https://odaneo.blob.core.windows.net/psd/3.png${azureSAS}`,
+          type: 'image/png',
+          storage: 'azure',
+          overwrite: true
+        }
+      ]
+    })
   }
 
   const handleCheckStatus = () => {
     setHtml('')
-    instance.get(api).then(res => {
-      setHtml(formatUtil.objToHTML(res.data))
-    })
+    instance.get(api)
   }
 
   const handleChangeSAS = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -227,7 +220,15 @@ function PS(): JSX.Element {
         Autotone an image
       </Button>
       {api && <Button onClick={handleCheckStatus}>Check status</Button>}
-      {html && <div dangerouslySetInnerHTML={{ __html: html }}></div>}
+      {html && (
+        <div
+          style={{
+            maxWidth: '1200px',
+            wordBreak: 'break-all'
+          }}
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
+      )}
     </div>
   )
 }
